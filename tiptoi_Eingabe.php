b35609b18f956fb55c9802ff28c72f00be14c2f2
feAtuRe <fe4tur3@gmx.net>
2026-06-11 15:31:23 +0200

    Update tiptoi_Eingabe.php

--- tiptoi_Eingabe.php
+++ tiptoi_Eingabe.php
@@ -1,6 +1,18 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "", "tiptoi");                  // Datenbankverbindung
+require_once __DIR__ . '/vendor/autoload.php';                              // Composer-Autoloader einbinden für:
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);                          // env.Datei mir Login-Daten laden
+$dotenv->load();
+
+$conn = new mysqli(                                                         // Variablen mit env-Inhalt verbinden
+    $_ENV['DB_HOST']
+    $_ENV['DB_USER']
+    $_ENV['DB_PASS']
+    $_ENV['DB_NAME']
+);
+
+
 
 if ($conn->connect_error) {                                             // Verbindung prüfen
     die("Verbindung fehlgeschlagen: " . $conn->connect_error);        // Abbrechen, wenn keine Verbindung
